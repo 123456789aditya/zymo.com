34c2faeb70c841b6f5848114f9f0ba92d886eaca
aditya pathak <pathakaditya881@gmail.com>
2025-01-26 19:30:36 +0530

    half-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*var heading=document.getElementById("btn-1");
+heading.addEventListener("click",display);*/
+
+function display(){
+    var head=document.innerHTML("demo");
+    head.innerHTML="hello world";
+
+}
