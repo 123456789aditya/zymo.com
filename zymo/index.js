ac4a1811d7344d0fcf96257270c3b39ac9bf29c3
aditya pathak <pathakaditya881@gmail.com>
2025-02-15 10:03:55 +0530

    message

--- zymo/index.js
+++ zymo/index.js
@@ -1,8 +1,23 @@
-/*var heading=document.getElementById("btn-1");
-heading.addEventListener("click",display);*/
+async function dataShow() {
+  let api = "http://localhost:3000/cardata";
+  let Table =
+    `<table border='1'><tr><th>Rollno</th><th>Name</th><th>City</th><th>Fees</th></tr>`;
 
-function display(){
-    var head=document.innerHTML("demo");
-    head.innerHTML="hello world";
+  const myObj = await fetch(api);
+  //console.log(myObj);
+  const Data = await myObj.json();
+  console.log(Data);
+  Data.map((item) => {
+    Table += `<tr>
+                         <td> ${item.carno} </td>
+                         <td> ${item.name} </td>
+                         <td> ${item.city} </td>
+                         <td> ${item.prices} </td>
+                       </tr> `;
+  });
+  Table += `</table>`  ;
+  document.getElementById("demoDisplay").innerHTML = Table;
 
 }
+
+dataShow()
